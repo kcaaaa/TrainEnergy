208fea6094dcfc9168148500fae76901753c5a0f
2473994475 <2473994475@qq.com>
2025-12-23 18:07:48 +0800

    修改路由模式为Hash模式以支持GitHub Pages

--- 演示系统/src/router/index.js
+++ 演示系统/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 // 导入组件
 import Home from '../components/Home/Home.vue'
@@ -62,7 +62,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
